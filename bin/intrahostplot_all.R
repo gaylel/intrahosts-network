e259d080e624b9c5d34160b7939dd63470c80333
gaylel <gaylel@p0.modelling>
2014-07-23 15:12:40 +0100

    summary plot functions

--- bin/intrahostplot_all.R
+++ bin/intrahostplot_all.R
@@ -12,22 +12,34 @@ source("ih_model3.R")
 source(paramfile)
 par_init <- ih_model_getparams(params.isopt, params.init)
 med <- NULL
+cn <- NULL
 for (inds in seq(1, 50))
 {
 	datafile <- paste(outdir, "/intrahost.", inds, ".RData", sep="")
-	if (file.exists(datafile))
+	if (file.exists(datafile) && inds!=27)
 	{
 		load(datafile)
-		colnames(mcmc.out) <- names(par_init)
-
 		mcmc.out <- exp(mcmc.out)
-		med <- rbind(med, apply((mcmc.out), 2, median))
+		#med <- rbind(med, apply((mcmc.out), 2, median))
+		med <- rbind(med, mcmc.out)
+		if (length(D$cn) == 1)		
+		cn <- rbind(cn, c(apply(mcmc.out , 2, median), D$cn, D$S, D$Ns))
 	}
 }
 
+colnames(med) <- names(par_init)
 histfile <- paste(outdir, "/mcmc.hist.pdf", sep="")
 
 pdf(histfile)
-for (i in seq(1, ncol(mcmc.out)))
-hist(mcmc.out[,i], main=colnames(mcmc.out)[i], xlab="", 50)
+for (i in seq(1, ncol(med)))
+hist(med[,i], main=colnames(med)[i], xlab="", 100)
+#print(med)
+
+colnames(cn) <- c(names(par_init), "cn", "S", "Ns")
+
+plot(cn[,"cn"], cn[,"t"])
+plot(cn[,"S"]/cn[,"Ns"], cn[,"t"])
+plot(cn[,"S"], cn[,"V0"])
+print(cn)
 dev.off()
+
